b2f87ddcf2641ed20a4de5d4983f1f7e087dfc4d
Liberty <rlkmandisodza@gmail.com>
2017-11-13 19:18:13 -0800

    total working for one object

--- app.js
+++ app.js
@@ -4,23 +4,24 @@ var FirstNPike = {
   max: 65,
   avg: 6.3,
   salesResults: [ ],
+  totalDay: 0,
   cookiesPerHour: function() {
   return Math.round((Math.random() * ( this.max - this.min) + this.min * this.avg));
   }
 }
 
-total = 0;
 for(var i = 6; i < 21; i++) {
   if(i < 13){
-    document.write(i + 'am: ' + FirstNPike.cookiesPerHour() + ' cookies\n');
-    total += total;
+    //document.write(i + 'am: ' + FirstNPike.cookiesPerHour() + ' cookies\n');
+    FirstNPike.totalDay += FirstNPike.cookiesPerHour();
     FirstNPike.salesResults.push(FirstNPike.cookiesPerHour());
   } else {
-    document.write(i + 'pm: ' + FirstNPike.cookiesPerHour() + ' cookies\n');
-    total += total;
+    //document.write(i + 'pm: ' + FirstNPike.cookiesPerHour() + ' cookies\n');
+    FirstNPike.totalDay += FirstNPike.cookiesPerHour();
     FirstNPike.salesResults.push(FirstNPike.cookiesPerHour());
   }
 }
+document.write(FirstNPike.totalDay);
 
   //document.write(total);
 
