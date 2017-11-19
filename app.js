3be427b40c2a9925020a0367d5f0fe250b29ddca
Liberty <rlkmandisodza@gmail.com>
2017-11-18 17:22:26 -0800

    working code for table with objecs rendering thier own row

--- app.js
+++ app.js
@@ -3,6 +3,29 @@
 var shopsArray = [];
 var storeOpTimes = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
+// The header row and footer row are each created in their own stand-alone function
+// Duplicate code has been removed and DRY principles are evident
+// Working on a non-master branch for the day, with regular commit history. Basically, every time you get something to work, you should do a commit. But you only need to push every couple of hours or so, tops.
+
+var headerRow = function() {
+  var tableElt = document.getElementById('store-tbl');
+  var tRowHeaderElt = document.createElement('tr');
+  var cellOneElt = document.createElement('td');
+  tRowHeaderElt.appendChild(cellOneElt);
+  cellOneElt.textContent = '';
+  for (var j = 0; j < storeOpTimes.length; j++) {
+    var opTimeCellElt = document.createElement('td');
+    opTimeCellElt.textContent = storeOpTimes[j];
+    tRowHeaderElt.appendChild(opTimeCellElt);
+  }
+  var cellTotElt = document.createElement('td');
+  cellTotElt.textContent = 'Total';
+  tRowHeaderElt.appendChild(cellTotElt);
+  tableElt.appendChild(tRowHeaderElt);
+};
+
+headerRow();
+
 function Store(name, min, max, avg) {
   this.name = name;
   this.min = min;
@@ -28,13 +51,8 @@ Store.prototype.cookiesPerDayArray = function() {
     //console.log('HourlyLog for Day: ', this.salesResults)
   }
 };
-
 // Good use of a constructor function; style and syntax are correctly implemented
-// Each cookie stand location should have a separate render() method that creates and appends its row to the table
-// The header row and footer row are each created in their own stand-alone function
-// Duplicate code has been removed and DRY principles are evident
-// Working on a non-master branch for the day, with regular commit history. Basically, every time you get something to work, you should do a commit. But you only need to push every couple of hours or so, tops.
-
+// Each cookie stand location should have a separate render() method that creates and appends //its row to the table
 Store.prototype.render = function (){
   var tableElt = document.getElementById('store-tbl');
   var tRowElt = document.createElement('tr');
@@ -46,6 +64,9 @@ Store.prototype.render = function (){
     infoCells.textContent = this.salesResults[i];
     tRowElt.appendChild(infoCells);
   }
+  var totCell = document.createElement('td');
+  totCell.textContent = this.totalDay;
+  tRowElt.appendChild(totCell);
   tableElt.appendChild(tRowElt);
 };
 
