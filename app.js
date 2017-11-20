75203276db1bd2588698c552fbb92c34b2dfbc5b
Liberty <rlkmandisodza@gmail.com>
2017-11-20 02:42:08 -0800

    web upload ready

--- app.js
+++ app.js
@@ -3,6 +3,57 @@
 var shopsArray = [];
 var storeOpTimes = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
+// The header row and footer row are each created in their own stand-alone function
+// Duplicate code has been removed and DRY principles are evident
+// Working on a non-master branch for the day, with regular commit history. Basically, every time you get something to work, you should do a commit. But you only need to push every couple of hours or so, tops.
+
+var headerRow = function() {
+  var tableElt = document.getElementById('store-tbl');
+  var tRowHeaderElt = document.createElement('tr');
+  var cellOneElt = document.createElement('td');
+  tRowHeaderElt.appendChild(cellOneElt);
+  cellOneElt.textContent = '';
+  for (var j = 0; j < storeOpTimes.length; j++) {
+    var opTimeCellElt = document.createElement('td');
+    opTimeCellElt.textContent = storeOpTimes[j];
+    tRowHeaderElt.appendChild(opTimeCellElt);
+  }
+  var cellTotElt = document.createElement('td');
+  cellTotElt.textContent = 'Total';
+  tRowHeaderElt.appendChild(cellTotElt);
+  tableElt.appendChild(tRowHeaderElt);
+};
+
+var grandTotal = function(array, time) {
+  var total = 0;
+  for (var k = 0; k < array.length; k++) {
+    total += shopsArray[k].salesResults[time];
+  }
+  return total;
+};
+
+var salCooTot = 0;
+var footerRow = function() {
+  var tableElt = document.getElementById('store-tbl');
+  var tRowFooterElt = document.createElement('tr');
+  var cellOneElt = document.createElement('td');
+  tRowFooterElt.appendChild(cellOneElt);
+  cellOneElt.textContent = 'Grand Total';
+  for (var j = 0; j < storeOpTimes.length; j++) {
+    var opTimeCellElt = document.createElement('td');
+    var salCoo = grandTotal(shopsArray, j);
+    salCooTot += salCoo;
+    opTimeCellElt.textContent = (salCoo);
+    tRowFooterElt.appendChild(opTimeCellElt);
+  }
+  var cellTotElt = document.createElement('td');
+  cellTotElt.textContent = salCooTot;
+  tRowFooterElt.appendChild(cellTotElt);
+  tableElt.appendChild(tRowFooterElt);
+};
+
+headerRow();
+
 function Store(name, min, max, avg) {
   this.name = name;
   this.min = min;
@@ -12,7 +63,7 @@ function Store(name, min, max, avg) {
   this.salesResults = [];
   shopsArray.push(this);
   this.cookiesPerDayArray();
-  //this.createTable();
+  this.render();
 }
 console.log('object constructor created: ', Store);
 
@@ -28,71 +79,25 @@ Store.prototype.cookiesPerDayArray = function() {
     //console.log('HourlyLog for Day: ', this.salesResults)
   }
 };
-
-Store.prototype.createTable = function() {
-  var tableEl = document.getElementById('store-tbl');
-  var header = this.createTableHead();
-  var body = this.createTableBody();
-  tableEl.appendChild(header);
-  tableEl.appendChild(body);
-};
-
-Store.prototype.createTableHead = function() {
-  var theadEl = document.createElement('thead');
-  var headRow = createTableRow('', storeOpTimes, 'Totals');
-  theadEl.appendChild(headRow);
-  return theadEl;
-};
-
-Store.prototype.createTableBody = function() {
-  var tbodyEl = document.createElement('tbody');
-
-  for(var k = 0; k < shopsArray.length; k++) {
-    var bodyRow = createTableRow(shopsArray[k].name, shopsArray[k].salesResults, shopsArray[k].totalDay);
-    tbodyEl.appendChild(bodyRow);
+// Good use of a constructor function; style and syntax are correctly implemented
+// Each cookie stand location should have a separate render() method that creates and appends //its row to the table
+Store.prototype.render = function (){
+  var tableElt = document.getElementById('store-tbl');
+  var tRowElt = document.createElement('tr');
+  var cellElt = document.createElement('td');
+  tRowElt.appendChild(cellElt);
+  cellElt.textContent = (this.name);
+  for (var i = 0; i < storeOpTimes.length; i++) {
+    var infoCells = document.createElement('td');
+    infoCells.textContent = this.salesResults[i];
+    tRowElt.appendChild(infoCells);
   }
-  return tbodyEl;
+  var totCell = document.createElement('td');
+  totCell.textContent = this.totalDay;
+  tRowElt.appendChild(totCell);
+  tableElt.appendChild(tRowElt);
 };
 
-//Calculates the Grand total sales prediction for Salmon Cookies - not rendered to table yet
-var grandTotal = 0;
-var overallDailyTotal = [];
-var calcOverallDailyTotal = function() {
-  console.log('the number of shops: ', storeOpTimes.length);
-  for(var n = 0; n < storeOpTimes.length; n++) {
-    var total = 0;
-    console.log('the number of hours to be summed: ', shopsArray.length);
-    for (var o = 0; o < shopsArray.length; o++){
-      total = total + shopsArray[o].salesResults[n];
-      console.log('Accumulation of busunesshour: ' + o, total);
-    }
-    grandTotal = grandTotal + total;
-    console.log('Grand Total: ' ,grandTotal);
-    overallDailyTotal.push(total);
-  }
-};
-
-
-function createTableRow(verticalHeader, dataPoints, verticalFooter) {
-  var trEl = document.createElement('tr');
-  var tdElOne = document.createElement('td');
-  tdElOne.textContent = verticalHeader;
-  trEl.appendChild(tdElOne);
-
-  for(var j = 0; j < dataPoints.length; j++) {
-    var tdElTwo = document.createElement('td');
-    tdElTwo.textContent = dataPoints[j];
-    trEl.appendChild(tdElTwo);
-  }
-
-  var tdElThree = document.createElement('td');
-  tdElThree.textContent = verticalFooter;
-  trEl.appendChild(tdElThree);
-
-  return trEl;
-}
-
-//createTable();
 var FirstNPike = new Store('FirstNPike', 23, 65, 6.3);
 console.log('instatiation of FirstNPike: ', FirstNPike);
 
@@ -107,48 +112,28 @@ console.log('instatiation of CapitolH: ', CapitolH);
 
 var Alki = new Store('Alki', 2, 16, 4.6);
 console.log('instatiation of Alki: ', Alki);
-Alki.createTable();
 
 console.log('ShopsArray is: ', shopsArray);
-calcOverallDailyTotal();
-console.log('Array of hourly totals: ', overallDailyTotal);
 
 //Event Handling
+//console.log('new store added into the array: ', stores);
 var formEl = document.getElementById('main-form');
 function onSubmit(event){
   event.preventDefault();
   console.log('event object Name: ', event.target[0].value);
-  var nwStore = event.target[0].value;
   console.log('event object max: ', event.target[1].value);
-  var nwCustMax = parseInt(event.target[1].value);
   console.log('event object min: ', event.target[2].value);
-  var nwCustMin = parseInt(event.target[2].value);
   console.log('event object avg: ', event.target[3].value);
-  var nwCustAvg = parseInt(event.target[3].value);
   console.log('the form was submitted');
-  var newShop = new Store(nwStore, nwCustMax, nwCustMin, nwCustAvg);
+  //Delete last row - the total row --- inner.HTML
+  var rowCount = document.getElementById('store-tbl').rows.length;
+  document.getElementById('store-tbl').deleteRow(rowCount - 1);
+  console.log('number of rows on table:', rowCount);
+  var newShop = new Store(event.target[0].value, event.target[1].value, event.target[2].value, event.target[3].value);
+  footerRow();
   console.log('New Shop instatiated and done.', newShop);
-  console.log('shopsArray is now: ' ,shopsArray);
-  addTableRow(nwStore, nwStore.salesResults, nwStore.totalDay);
-}
-
-function addTableRow(location, hourlySales, totalSales) {
-  var table = document.getElementById('store-tbl');
-  var trEl = document.createElement('tr');
-  table.appendChild(trEl);
-  var tdElFirst = document.createElement('td');
-  tdElFirst.textContent = location;
-  trEl.appendChild(tdElFirst);//.......Done
-
-  for (var m = 0; m < storeOpTimes.length; m++) {
-    var tdElvalues = document.createElement('td');
-    tdElvalues.textContent = shopsArray[shopsArray.length - 1].salesResults[m];
-    trEl.appendChild(tdElvalues);
-  }
-  var tdElThird = document.createElement('td');
-  tdElThird.textContent = totalSales;
-  trEl.appendChild(tdElThird);
-
-  return trEl;
 }
 formEl.addEventListener('submit', onSubmit);
+
+
+footerRow();
